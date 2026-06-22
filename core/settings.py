9fec57e13dde158441adece237260d0da445a1da
danieceiflora <daniel_sousa_marques@hotmail.com>
2026-06-22 13:03:26 -0300

    fix: update DEBUG setting to use environment variable

--- core/settings.py
+++ core/settings.py
@@ -17,9 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-only-change-in-production')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-##DEBUG = True if os.environ.get('DEBUG', 'True').lower() in ('1', 'true', 'yes', 'on') else False
-DEBUG = False
+DEBUG=os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
